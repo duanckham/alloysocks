f48e0f82439f0cd1bd763df116f84eeef3e631ee
noevil <noevil@foxmail.com>
2014-06-13 18:53:40 +0800

    update package.json

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var https = require('https');
 var http = require('http');
+var router = require('./lib/router');
 var app = express();
 
 var options = {
 	key: fs.readFileSync('ca/alloysocks-key.pem'),
 	cert: fs.readFileSync('ca/alloysocks-cert.pem')
 };
+
+app.use('/', router);
 
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
